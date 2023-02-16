a237ef71326b41f10a26dc9e5aa0a01e7e79b4f7
Alwin <alwin5177@gmail.com>
2023-02-16 17:47:09 +0530

    Second Commit

--- src/Selenium/alertBox.java
+++ src/Selenium/alertBox.java
@@ -20,24 +20,24 @@ public class alertBox {
 
         WebElement alertBox1=driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[1]/div/div/button"));
         alertBox1.click();
-        Alert alert=driver.switchTo().alert();
-        alert.accept();
+        Alert alert1=driver.switchTo().alert();
+        alert1.accept();
 
         WebElement alertBox2=driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[2]/div/div/button"));
         alertBox2.click();
-        alert=driver.switchTo().alert();
-        alert.accept();
+        Alert alert2=driver.switchTo().alert();
+        alert2.accept();
 
         WebElement alertBox3=driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[3]/div/div/button"));
         alertBox3.click();
-        alert=driver.switchTo().alert();
-        alert.sendKeys(" "+"Alwin ok");
-        alert.accept();
+        Alert alert3=driver.switchTo().alert();
+        alert3.sendKeys(" "+"Alwin ok");
+        alert3.accept();
 
         WebElement alertBox4=driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[4]/div/div/button"));
         alertBox4.click();
-        alert=driver.switchTo().alert();
-        alert.accept();
+        Alert alert4=driver.switchTo().alert();
+        alert4.accept();
 
     }
 }
